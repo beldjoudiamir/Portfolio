213b5c63c1bbce6ac349e3b7cc7540fa18aa6929
beldjoudiamir <mr.beldjoudi.amir@gmail.com>
2023-10-09 00:56:05 +0200

    reactjs champ contrôle avec le responsive

--- projetPerso/scrollPage/src/Component/AudioPlayer/index.jsx
+++ projetPerso/scrollPage/src/Component/AudioPlayer/index.jsx
@@ -1,51 +1,161 @@
-import "./style.scss";
-import albumMusic from "../../../public/albumMusic/61+PrnVotwL._UF894,1000_QL80_.jpg";
+import React, { useState, useRef, useEffect } from "react";
+import track1 from "../../../public/Music/Aurora.mp3";
+import track2 from "../../../public/Music/X2Download.app - LP-Lost On You-Lyrics (128 kbps).mp3";
+import track3 from "../../../public/Music/Solar.mp3";
+import albumMusic from "../../../public/Artist/61+PrnVotwL._UF894,1000_QL80_.jpg";
 import play from "../../../public/play-icon.svg";
 import shuffle from "../../../public/shuffle-icon.svg";
 import next from "../../../public/next-icon.svg";
 import prev from "../../../public/prev-icon.svg";
+import pause from "../../../public/pause-icon.svg";
+import "./style.scss";
+const tracks = [
+  { title: "track1", artist: "Betical", id: 1 },
+  { title: "track2", artist: "Betical", id: 2 },
+  { title: "track", artist: "Betical", id: 1 },
+];
 
-function audioPlayer() {
+function AudioPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const audioRef = useRef(null);
+
+  const togglePlayPause = () => {
+    const audio = audioRef.current;
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const playNextTrack = () => {
+    const newIndex = (currentTrackIndex + 1) % tracks.length;
+    setCurrentTrackIndex(newIndex);
+  };
+
+  const playPrevTrack = () => {
+    const newIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+    setCurrentTrackIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.src = tracks[currentTrackIndex];
+    audio
+      .play()
+      .then(() => {
+        setIsPlaying(true);
+      })
+      .catch((error) => {
+        setIsPlaying(false);
+      });
+  }, [currentTrackIndex]);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    const updateProgress = () => {
+      const progressValue = (audio.currentTime / audio.duration) * 100;
+      setProgress(progressValue);
+    };
+
+    audio.addEventListener("timeupdate", updateProgress);
+
+    return () => {
+      audio.removeEventListener("timeupdate", updateProgress);
+    };
+  }, []);
+
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className=" bg-red-500 m-24 p-1 rounded-lg">
-        <div className="flex justify-center">
-          <img src={albumMusic} alt="" className="w-1/2 h-1/2" />
-        </div>
-        <div className="pt-5 pr-12 pb-12 pl-12">
-          <div className="flex justify-between">
-            <div className="topLetfContent">
-              <h1 className=" text-black text-xl">Tiki taka</h1>
-              <p className=" text-gray-500 font-bold">Vacara</p>
-              <p className=" text-gray-400 font-thin text-sm mt-auto mb-0.5">
-                <span>1</span>/4
-              </p>
+    <>
+      <div className="h-screen flex lg:items-center  lg:justify-center m-10">
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex-grow  bg-red-500 w-3/12 p-1 rounded-lg md:m-12 lg:p-4">
+            <div className="flex justify-center mt-5">
+              <img
+                src={albumMusic}
+                alt=""
+                className="w-1/2 h-1/2 md:w-1/4 lg:h-3/4"
+              />
+            </div>
+            <div className="pt-5 pr-12 pb-12 pl-12 md:pl-5 lg:pr-6">
+              <div className="flex justify-between">
+                <div className="topLetfContent">
+                  <h1 className=" text-black text-xl">{tracks.titel}</h1>
+                  <p className=" text-gray-500 font-bold">Vxxx</p>
+                  <p className=" text-gray-400 font-thin text-sm mt-auto mb-0.5">
+                    <span>1</span>/4
+                  </p>
+                </div>
+              </div>
+
+              <audio ref={audioRef}></audio>
+              <div className=" relative left-0 m-8 mt-9 mb-15 bg-gray-600  rounded cursor-pointer ">
+                <div className="progress-container">
+                  <div
+                    className="progress-bar"
+                    style={{ width: `${progress}%` }}
+                  ></div>
+                </div>
+                <span className="absolute top-2.5 text-sm text-gray-500 pointer-events-none right-0"></span>
+                <span className="absolute top-2.5 text-sm text-gray-500 pointer-events-none right-0"></span>
+              </div>
+              <nav className="relative flex justify-center items-center">
+                <button className="absolute left-0">
+                  <img
+                    src={shuffle}
+                    alt="shuffleIcone"
+                    className="w-5 text-gray-600 transition-fill duration-200 ease-in-out"
+                  />
+                </button>
+                <button
+                  onClick={playPrevTrack}
+                  className="bg-transparent border-0 cursor-pointer focus:outline-none"
+                >
+                  <img
+                    src={prev}
+                    alt="prevIcone"
+                    className="w-6 pointer-events-none"
+                  />
+                </button>
+                <button
+                  onClick={togglePlayPause}
+                  className="flex justify-center items-center h-16 w-16 rounded-full mx-3.5"
+                >
+                  {isPlaying ? (
+                    <img
+                      src={pause}
+                      alt="playIcone"
+                      className=" w-full pointer-events-none"
+                    />
+                  ) : (
+                    <img
+                      src={play}
+                      alt="playIcone"
+                      className=" w-full pointer-events-none"
+                    />
+                  )}
+                </button>
+                <button
+                  onClick={playNextTrack}
+                  className="bg-transparent border-0 cursor-pointer focus:outline-none"
+                >
+                  <img
+                    src={next}
+                    alt="nextIcone"
+                    className="w-6 pointer-events-none"
+                  />
+                </button>
+              </nav>
             </div>
           </div>
-
-          <audio src="../../../public/albumMusic/X2Download.app - LP-Lost On You-Lyrics (128 kbps).mp3"></audio>
-          <div className="ProgressContainer">
-            <div className="progressBar"></div>
-            <span className="timeIndicateur currentTime"></span>
-            <span className="timeIndicateur durationTime"></span>
-          </div>
-          <nav className="navigation">
-            <button className="shuffle w-auto">
-              <img src={shuffle} alt="shuffleIcone" />
-            </button>
-            <button className="prevBtn w-6">
-              <img src={prev} alt="prevIcone" />
-            </button>
-            <button className="playBtn w-auto">
-              <img src={play} alt="playIcone" />
-            </button>
-            <button className="nextBtn w-6">
-              <img src={next} alt="nextIcone" />
-            </button>
-          </nav>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-export default audioPlayer;
+
+export default AudioPlayer;
