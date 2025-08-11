59efa8324d4c6bd2b52ec941578efa027bb14f5b
amir.beldjoudi <110815631+beldjoudiamir@users.noreply.github.com>
2025-08-11 11:33:23 +0200

    Delete projetPerso/octoTrans directory

--- projetPerso/octoTrans/src/Component/Error/index.js
+++
@@ -1,9 +0,0 @@
-import noFound from "../../Assets/erreur-404.png";
-import "./style.scss";
-export default function Erreur() {
-  return (
-    <div className="erreurPage">
-      <img className="helloPage" src={noFound} alt="No Found" />
-    </div>
-  );
-}
